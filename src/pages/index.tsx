7f1345ba157d758bbe9ae00466545718e7d128f1
ke3TAro2037 <ke3taro2037@gmail.com>
2025-03-31 16:43:31 +0900

    Fix Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,14 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          MBFサーバーのガイドブックへようこそ！
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/はじめに">
+            ガイドブックを開く
           </Link>
         </div>
       </div>
@@ -32,12 +32,9 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="MBFサーバーの新規さん向けガイドブック">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
